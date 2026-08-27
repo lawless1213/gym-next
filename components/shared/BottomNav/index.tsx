ddc0b4def28edd3d43005355b3f10ebcf50b6c2a
lawless1213 <lototskiy.dev@gmail.com>
2026-08-27 11:26:29 +0300

    navbar fix

--- components/shared/BottomNav/index.tsx
+++ components/shared/BottomNav/index.tsx
@@ -17,7 +17,7 @@ export function BottomNav() {
   const pathname = usePathname();
   return (
     <nav className="fixed bottom-0 left-0 right-0 z-50 border-t border-border bg-card/95 backdrop-blur-lg">
-      <div className="mx-auto flex h-12 max-w-lg items-center justify-around px-4">
+      <div className="mx-auto flex max-w-lg items-center justify-around px-4">
         {getNavLinks(!!user).map((item: NavItem) => {
           const isActive = pathname === item.link;
           return (
@@ -26,19 +26,19 @@ export function BottomNav() {
                 {item.link ? (
                   <Link
                     href={item.link}
-                    className={`flex min-w-16 flex-col items-center gap-1 rounded-xl px-4 py-2 transition-all duration-200 ${isActive ? "text-primary pointer-events-none cursor-none" : "text-muted-foreground hover:text-foreground"}`}
+                    className={`flex min-w-16 h-16 justify-center flex-col items-center gap-1 rounded-xl px-4 py-2 transition-all duration-200 ${isActive ? "text-primary pointer-events-none cursor-none" : "text-muted-foreground hover:text-foreground"}`}
                     aria-label={item.label}>
                     <div className={`relative ${isActive ? "scale-130" : ""} transition-transform`}>
-                      <item.icon className={`h-6 w-6`} />
+                      <item.icon className={`size-6`} />
                     </div>
                   </Link>
                 ) : ( item.modal &&
                   <div
                     onClick={() => open(item.modal)}
-                    className={`flex min-w-16 flex-col items-center gap-1 rounded-xl px-4 py-2 transition-all duration-200 ${isActive ? "text-primary pointer-events-none cursor-none" : "text-muted-foreground hover:text-foreground"}`}
+                    className={`flex min-w-16 h-16 justify-center flex-col items-center gap-1 rounded-xl px-4 py-2 transition-all duration-200 ${isActive ? "text-primary pointer-events-none cursor-none" : "text-muted-foreground hover:text-foreground"}`}
                     aria-label={item.label}>
                     <div className={`relative ${isActive ? "scale-130" : ""} transition-transform`}>
-                      <item.icon className={`size-6`} />
+                      <item.icon className={`size-10`} />
                     </div>
                   </div>
                 )}
@@ -48,7 +48,6 @@ export function BottomNav() {
           );
         })}
       </div>
-      {/* Safe area for iOS */}
       <div className="h-safe-area-inset-bottom bg-card" />
     </nav>
   );
