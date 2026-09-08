885f39f523a32c29fba9dfd30198e42eeba5388e
lawless1213 <lototskiy.dev@gmail.com>
2026-09-08 18:30:24 +0300

    implement user editing

--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -1,19 +1,25 @@
 import { db } from "@/lib/config/firebaseConfig";
 import { doc, setDoc } from "firebase/firestore";
+import type { UserParams } from "@/lib/services/user";
 
-interface userParamsProps {
-  param: "theme" | "language" | "gender" | "distance" | "weight";
-  value: string;
+type UserParamKey = keyof UserParams;
+
+type SetUserParamsArgs<K extends UserParamKey> = {
+  param: K;
+  value: UserParams[K];
   userId?: string;
-}
+};
 
-export async function setUserParams({ param, value, userId }: userParamsProps) {
+export async function setUserParams<K extends UserParamKey>({
+  param,
+  value,
+  userId,
+}: SetUserParamsArgs<K>) {
   if (!userId) {
     console.error("setUserParams: userId is missing");
     return;
   }
 
-  console.log(param + "-" + value);
   const userRef = doc(db, "users", userId);
 
   await setDoc(userRef, { [param]: value }, { merge: true });
