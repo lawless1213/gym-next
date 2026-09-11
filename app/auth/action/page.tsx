7bef11c4d9a40bf96e831b528227b3298a2df3e3
lawless1213 <lototskiy.dev@gmail.com>
2026-09-11 11:28:21 +0300

    implement user change email

--- app/auth/action/page.tsx
+++ app/auth/action/page.tsx
@@ -6,15 +6,13 @@ import { applyActionCode } from "firebase/auth";
 import { auth } from "@/lib/config/firebaseConfig";
 import { Button } from "@/components/ui/Button";
 import { useTranslations } from "next-intl";
+import { useModal } from "@/components/modals/modal-store";
 
 export default function AuthActionPage() {
   const t = useTranslations("auth.page");
-
+  const { open } = useModal();
   const searchParams = useSearchParams();
-  const router = useRouter();
-
   const [status, setStatus] = useState<"loading" | "success" | "error">("loading");
-
   const mode = searchParams.get("mode");
   const oobCode = searchParams.get("oobCode");
 
@@ -45,7 +43,7 @@ export default function AuthActionPage() {
   if (status === "loading") {
     return (
       <div className="flex flex-col items-center justify-center ">
-        <p>{t('confirming')}</p>
+        <p>{t("confirming")}</p>
       </div>
     );
   }
@@ -53,18 +51,18 @@ export default function AuthActionPage() {
   if (status === "error") {
     return (
       <div className="flex flex-col items-center justify-center space-y-4">
-        <h1 className="text-xl font-bold text-error">{t('confirmingError')}</h1>
-        <p>{t('linkOld')}</p>
-        <Button onClick={() => router.push("/login")}>{t('login')}</Button>
+        <h1 className="text-xl font-bold text-error">{t("confirmingError")}</h1>
+        <p>{t("linkOld")}</p>
+        <Button onClick={() => open("auth")}>{t("login")}</Button>
       </div>
     );
   }
 
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
-      <h1 className="text-2xl font-bold text-success">{t('confirmingSuccess')}</h1>
-      <p>{t('repeatLogin')}</p>
-      <Button onClick={() => router.push("/login")}>{t('login')}</Button>
+      <h1 className="text-2xl font-bold text-success">{t("confirmingSuccess")}</h1>
+      <p>{t("repeatLogin")}</p>
+      <Button onClick={() => open("auth")}>{t("login")}</Button>
     </div>
   );
 }
